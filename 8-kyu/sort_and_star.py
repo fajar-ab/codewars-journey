cbd5c5ac4b2eb9d81a2429a043d49dab506284ba
fajar-ab <fajarfanz378@gmail.com>
2026-05-01 21:47:41 +0700

    solve: [8 kyu] Sort and Star - https://www.codewars.com/kata/57cfdf34902f6ba3d300001e

---
+++ 8-kyu/sort_and_star.py
@@ -0,0 +1,6 @@
+# Rank  : 8 kyu
+# Title : Sort and Star
+# URL   : https://www.codewars.com/kata/reviews/5af2a141f82f0221f70004c7/groups/5af2acbf7b755512b000201f
+
+def two_sort(array):
+    return "***".join(sorted(array)[0])
