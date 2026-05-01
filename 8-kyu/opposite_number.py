55ae5d86da0ca698dbf0865708ef793102b81a14
fajar-ab <fajarfanz378@gmail.com>
2026-05-02 01:24:36 +0700

    solve: [8 kyu] Opposite number - https://www.codewars.com/kata/56dec885c54a926dcd001095

---
+++ 8-kyu/opposite_number.py
@@ -0,0 +1,7 @@
+# Rank  : 9 kyu
+# Title : Opposite number
+# URL   : https://www.codewars.com/kata/reviews/56deebdf6a5c28baa900003b/groups/56deebdf6a5c28baa900003d
+
+def opposite(number):
+    return -number
+
