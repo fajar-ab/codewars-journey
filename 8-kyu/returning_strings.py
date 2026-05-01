abba232adbedb3344f19be6bb04a41f394429f02
fajar-ab <fajarfanz378@gmail.com>
2026-05-02 00:57:02 +0700

    solve: [8 kyu] Returning String - https://www.codewars.com/kata/55a70521798b14d4750000a4

---
+++ 8-kyu/returning_strings.py
@@ -0,0 +1,6 @@
+# Rank  : 8 kyu
+# Title : Returning Strings
+# URL   : https://www.codewars.com/kata/reviews/55a709464d8e6220280000c9/groups/5a3bd143faaf5a575f000006
+
+def greet(name):
+    return f"Hello, {name} how are you doing today?"
