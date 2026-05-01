f3da56fd4bc801ceeb76db54029ae4eec7b3d8c9
fajar-ab <fajarfanz378@gmail.com>
2026-05-02 00:17:10 +0700

    refaktor: [7 kyu] String ends with? | ubah dari perbandingan menggunakan string slicing menjadi pengecekan method str.endswith() lebih efektif  - https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d

--- 7-kyu/string_ends_with.py
+++ 7-kyu/string_ends_with.py
@@ -3,4 +3,4 @@
 # URL   : https://www.codewars.com/kata/reviews/5463ab67541c67d9b0000064/groups/63aeb37a3f20520001724ac2
 
 def solution(text, ending):    
-    return ending == text[-(len(ending)):]
+    return text.endswith(ending)
