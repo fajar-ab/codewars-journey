29375cdd8c14d58186aaa737d0190f9fe281dc8e
fajar-ab <fajarfanz378@gmail.com>
2026-05-01 22:11:34 +0700

    solve: [8 kyu] Area of a Square - https://www.codewars.com/kata/5748838ce2fab90b86001b1a

---
+++ 8-kyu/area_of_a_square.py
@@ -0,0 +1,8 @@
+# Rank  : 8 kyu
+# Title : Area of a Square
+# URL   : https://www.codewars.com/kata/reviews/5dc4e7e4b9b5a2000185f694/groups/69dc9cbcefe1617c07285a4c
+
+from math import pi
+
+def square_area(a):
+    return (2 * a / pi) ** 2
