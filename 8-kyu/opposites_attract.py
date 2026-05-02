24d52aedd7bfb8eafc992cadd82f1400864aa448
fajar-ab <fajarfanz378@gmail.com>
2026-05-02 18:21:05 +0700

    refaktor [8 kyu] Opposites Attract | menggunakan nilai dari hasil modulo untuk mempresentasikan nilai boolean - https://www.codewars.com/kata/555086d53eac039a2a000083

--- 8-kyu/opposites_attract.py
+++ 8-kyu/opposites_attract.py
@@ -1,6 +1,6 @@
 # Rank  : 8 kyu
 # Title : Opposites Attract
-# Link  : https://www.codewars.com/kata/reviews/555acb6d4f0c35acb5000084/groups/555ae05e8e3075179300005a
+# Link  : https://www.codewars.com/kata/reviews/555acb6d4f0c35acb5000084/groups/555acb6d4f0c358f9500008a
 
 def lovefunc( flower1, flower2 ):
-    return (flower1 + flower2) % 2 == 1
+    return (flower1 + flower2) % 2
