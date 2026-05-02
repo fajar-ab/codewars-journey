df63c8b7250b3b454fd7e9edde558f2b56552053
fajar-ab <fajarfanz378@gmail.com>
2026-05-02 16:42:21 +0700

    solve: [8 kyu] Square(n) Sum - https://www.codewars.com/kata/515e271a311df0350d00000f

---
+++ 8-kyu/square_n_sum.py
@@ -0,0 +1,6 @@
+# Rank  : 8 kyu
+# Title : Square n Sum
+# Link  : https://www.codewars.com/kata/reviews/55246c58de8b4bcd960003a1/groups/552fcd7407ed6ac9ec00001d
+
+def square_sum(numbers):
+    return sum(number ** 2 for number in numbers)
