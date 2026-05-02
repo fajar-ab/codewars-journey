5f9df67b21ac89377e4581b902d278d860e5721f
fajar-ab <fajarfanz378@gmail.com>
2026-05-02 16:38:51 +0700

    solve: [8 kyu] Sum of positive - https://www.codewars.com/kata/5715eaedb436cf5606000381

---
+++ 8-kyu/sum_of_positive.py
@@ -0,0 +1,6 @@
+# Rank  : 8 kyu
+# Title : Sum of positive
+# Link  : https://www.codewars.com/kata/reviews/571601f0cc5b1c5cd8000020/groups/58333da80474d1c09d0004ed
+
+def positive_sum(arr):
+    return sum([number for number in arr if number > 0])
