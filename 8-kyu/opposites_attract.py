329dd2f3599150838a0f2539584bd1f881911923
fajar-ab <fajarfanz378@gmail.com>
2026-05-02 18:02:20 +0700

    solve [8 kyu] Opposites Attract - https://www.codewars.com/kata/555086d53eac039a2a000083

---
+++ 8-kyu/opposites_attract.py
@@ -0,0 +1,6 @@
+# Rank  : 8 kyu
+# Title : Opposites Attract
+# Link  : https://www.codewars.com/kata/reviews/555acb6d4f0c35acb5000084/groups/555ae05e8e3075179300005a
+
+def lovefunc( flower1, flower2 ):
+    return (flower1 + flower2) % 2 == 1
