40dcf6c99cf442f934d577458dc0ddc3bc738799
fajar-ab <fajarfanz378@gmail.com>
2026-05-03 20:17:59 +0700

    solve [8 kyu] String repeat - https://www.codewars.com/kata/57a0e5c372292dd76d000d7e

---
+++ 8-kyu/string_repeat.py
@@ -0,0 +1,6 @@
+# Rank  : 8 kyu
+# Title : String repeat
+# Link  : https://www.codewars.com/kata/reviews/57a98e6aaa22319409000072/groups/69f7472bb59144ca91f0eef9
+
+def repeat_str(repeat, string):
+    return string * repeat
