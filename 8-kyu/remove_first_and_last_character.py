8e738037cddf7d92728bd7d876bc7d05bfca99d4
fajar-ab <fajarfanz378@gmail.com>
2026-05-03 20:16:11 +0700

    solve [8 kyu] Remove First and Last Character - https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0

---
+++ 8-kyu/remove_first_and_last_character.py
@@ -0,0 +1,6 @@
+# Rank  : 8 kyu
+# Title : Remove First and Last Character
+# Link  : https://www.codewars.com/kata/reviews/56bf5c369ecef79fc1000076/groups/56bf66d369f4e88835000cee
+
+def remove_char(s):
+    return s[1:-1]
