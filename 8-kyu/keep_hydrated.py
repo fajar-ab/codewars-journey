14e1fd4f2989a855c030d44905ef5582ea8f0406
fajar-ab <fajarfanz378@gmail.com>
2026-05-03 13:25:19 +0700

    solve [8 kyu] Keep Hydrated! - https://www.codewars.com/kata/582cb0224e56e068d800003c

---
+++ 8-kyu/keep_hydrated.py
@@ -0,0 +1,6 @@
+# Rank  : 8 kyu
+# Title : Keep Hydrated!
+# Link  : https://www.codewars.com/kata/reviews/582e00ba4b1368af7b0000b3/groups/582e64563a0b2b545c000146
+
+def litres(time):
+    return (time * 0.5) // 1
