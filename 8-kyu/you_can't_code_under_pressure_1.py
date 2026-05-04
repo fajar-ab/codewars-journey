61bebe8144d8029ebf826c14b8334f8dc8b36454
fajar-ab <fajarfanz378@gmail.com>
2026-05-04 18:32:50 +0700

    solve [8 kyu] - You Can't Code Under Pressure #1 - https://www.codewars.com/kata/53ee5429ba190077850011d4

---
+++ 8-kyu/you_can't_code_under_pressure_1.py
@@ -0,0 +1,6 @@
+# Rank  : 8 kyu
+# Title : You Can't Code Under Pressure #1
+# Link  : https://www.codewars.com/kata/reviews/5411f6d717dc0b0f8700021b/groups/5bfd68c4ff3986154d000412
+
+def double_integer(i):
+    return i+i
