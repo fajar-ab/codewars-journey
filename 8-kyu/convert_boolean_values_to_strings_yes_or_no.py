1c30659f3fadd00976aab04e19b3c321210997d9
fajar-ab <fajarfanz378@gmail.com>
2026-05-04 10:58:04 +0700

    refaktor [8 kyu] Convert boolean values to strings 'Yes' or 'No' - https://www.codewars.com/kata/53369039d7ab3ac506000467

--- 8-kyu/convert_boolean_values_to_strings_yes_or_no.py
+++ 8-kyu/convert_boolean_values_to_strings_yes_or_no.py
@@ -1,9 +1,11 @@
 # Rank  : 8 kyu
 # Title : Convert boolean values to strings 'Yes' or 'No'
-# Link  : https://www.codewars.com/kata/reviews/545878a4888e9859aa000210/groups/5825f8b422be6eec6400002b
+# Link  : https://www.codewars.com/kata/reviews/545878a4888e9859aa000210/groups/545a7a3085166ae255000aae
 
 def bool_to_word(boolean):
-    return "Yes" if boolean else "No"
+    """dapat menggunakan nilai bolean untuk mengakses index di dalam list
+    namun cara ini hanya bisa jika length dari list sama dengan 2"""
+    return ["No", "Yes"][boolean]
 
 print(bool_to_word(True))
 print(bool_to_word(False))
