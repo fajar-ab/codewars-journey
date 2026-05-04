64e299575efecde509dc0b9fe2b8a89e2f035f6f
fajar-ab <fajarfanz378@gmail.com>
2026-05-04 17:56:15 +0700

    solve [8 kyu] Convert a Number to a String! - https://www.codewars.com/kata/5265326f5fda8eb1160004c8

---
+++ 8-kyu/convert_a_number_to_a_string.py
@@ -0,0 +1,6 @@
+# Rank  : 8 kyu
+# Title : Convert a Number to a String
+# Link  : https://www.codewars.com/kata/reviews/54272ba8e80d824cb80001f7/groups/69f87a442f964a949fe8d44b
+
+def number_to_string(num):
+    return str(num)
