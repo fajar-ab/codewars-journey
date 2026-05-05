8a62bb13f99cbd2ac6d0afcde14804519906a748
fajar-ab <fajarfanz378@gmail.com>
2026-05-06 01:19:15 +0700

    refaktor [8 kyu] Counting sheep... | menggunakan list.count(x) - https://www.codewars.com/kata/54edbc7200b811e956000556

--- 8-kyu/counting_sheep_.py
+++ 8-kyu/counting_sheep_.py
@@ -1,6 +1,7 @@
 # Rank  : 8 kyu
 # Title : Counting sheep...
-# Link  : https://www.codewars.com/kata/reviews/550af10c9c1f8949220001f9/groups/61fd11e864a4c200019e893a
+# Link  : https://www.codewars.com/kata/reviews/550af10c9c1f8949220001f9/groups/5702872ccc2e25f43a000cdc
 
 def count_sheeps(sheep):
-    return len([s for s in sheep if s])
+    """list.count(x) menghitung berapa x di list"""
+    return sheep.count(True)
