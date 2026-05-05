878e0c6edee7c452dcc205dbbdcfe06e0c15dc10
fajar-ab <fajarfanz378@gmail.com>
2026-05-05 23:00:42 +0700

    solve [8 kyu] Beginner - Lost Without a Map - https://www.codewars.com/kata/57f781872e3d8ca2a000007e

---
+++ 8-kyu/beginner_lost_without_a_map.py
@@ -0,0 +1,6 @@
+# Rank  : 8 kyu
+# Title : Beginner - Lost Without a Map
+# Link  : https://www.codewars.com/kata/reviews/57fbf74b777d18094c0000e4/groups/57fbfaa4dff6dfc1a5000078``
+
+def maps(a):
+    return list(map(lambda b: b * 2, a))
