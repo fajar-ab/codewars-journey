373b0980c9c9322ec758770a29aed2551e5bdb1b
fajar-ab <fajarfanz378@gmail.com>
2026-05-05 22:09:42 +0700

    solve [8 kyu] Return Negative - https://www.codewars.com/kata/55685cd7ad70877c23000102

---
+++ 8-kyu/return_negative.py
@@ -0,0 +1,6 @@
+# Rank  : 8 kyu
+# Title : Return Negative
+# Link  : https://www.codewars.com/kata/reviews/556a410ed44ca75d15000023/groups/55a0140b0f11df589300014c
+
+def make_negative(number):
+    return number if number <= 0 else number * -1
