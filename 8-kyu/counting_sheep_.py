8fc995473c82bcae42f522f3bca630f51d10e8d3
fajar-ab <fajarfanz378@gmail.com>
2026-05-06 01:07:39 +0700

    solve [8 kyu] Counting sheep... - https://www.codewars.com/kata/54edbc7200b811e956000556

---
+++ 8-kyu/counting_sheep_.py
@@ -0,0 +1,6 @@
+# Rank  : 8 kyu
+# Title : Counting sheep...
+# Link  : https://www.codewars.com/kata/reviews/550af10c9c1f8949220001f9/groups/61fd11e864a4c200019e893a
+
+def count_sheeps(sheep):
+    return len([s for s in sheep if s])
