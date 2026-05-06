3307f7a306fe966b69796b4da8ef750ef6dc75e7
fajar-ab <fajarfanz378@gmail.com>
2026-05-06 20:31:24 +0700

    solve [8 kyu] Simple multiplication - https://www.codewars.com/kata/583710ccaa6717322c000105

---
+++ 8-kyu/simple_multiplication.py
@@ -0,0 +1,6 @@
+# Rank  : 8 kyu
+# Title : Simple multiplication
+# Link  : https://www.codewars.com/kata/reviews/5b348e41ccc956645000142d/groups/5b36a431715b374e82000363
+
+def simple_multiplication(number) :
+    return number * [8, 9][number % 2]
