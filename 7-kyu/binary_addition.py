88610d600b77afb5efd4d5191c6e0f8bb5041cf4
fajar-ab <fajarfanz378@gmail.com>
2026-05-06 18:38:52 +0700

    solve [7 kyu] Binary Addition - https://www.codewars.com/kata/551f37452ff852b7bd000139

---
+++ 7-kyu/binary_addition.py
@@ -0,0 +1,6 @@
+# Rank  : 7 kyu
+# Title : Binary Addition
+# Link  : https://www.codewars.com/kata/reviews/565f42510b9d78fa10000069/groups/5cd32f0a7c59b20001dbc656
+
+def add_binary(a,b):
+    return f"{a+b:b}"
