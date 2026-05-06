277c03106f82e97cd556f4cf3d94d82fce6eb648
fajar-ab <fajarfanz378@gmail.com>
2026-05-06 20:36:25 +0700

    solve [8 kyu] Sum Arrays - https://www.codewars.com/kata/53dc54212259ed3d4f00071c

---
+++ 8-kyu/sum_arrays.py
@@ -0,0 +1,9 @@
+# Rank  : 8 kyu
+# Title : Sum Arrays
+# Link  : https://www.codewars.com/kata/reviews/54cd6ef118cae04721000085/groups/54d30cda525c157bea000053
+
+sum_array = sum
+
+print(sum_array([1, 5.2, 4, 0, -1]))
+print(sum_array([-2.398]))
+print([])
