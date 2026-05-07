6673acadf496777468291b92e671aba34f176f24
fajar-ab <fajarfanz378@gmail.com>
2026-05-08 00:41:34 +0700

    solve: DNA to RNA Conversion (8 kyu)
    https://www.codewars.com/kata/reviews/555628327302c4fd84000071/groups/5ec9d358a200d2000197f2f4

---
+++ 8-kyu/dna_to_rna_conversion.py
@@ -0,0 +1,7 @@
+# Rank  : 8 kyu
+# Title : DNA to RNA Conversion
+# Link  : https://www.codewars.com/kata/5556282156230d0e5e000089
+
+def dna_to_rna(dna):
+    rna = dna.replace("T", "U")
+    return rna
