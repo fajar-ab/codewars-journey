cc721eadf338818893d4ce6593695fae83878823
fajar-ab <fajarfanz378@gmail.com>
2026-05-08 00:50:23 +0700

    solve: A Needle in the Haystack (8 kyu)
    https://www.codewars.com/kata/reviews/568304e7c0c0e6d62000005f/groups/5b7f0a4fbb83b399df001a79

---
+++ 8-kyu/a_needle_in_the_haystack.py
@@ -0,0 +1,6 @@
+# Rank  : 8 kyu
+# Title : A Needle in the Haystack
+# Link  : https://www.codewars.com/kata/56676e8fabd2d1ff3000000c
+
+def find_needle(haystack):
+    return f"found the needle at position {haystack.index('needle')}"
