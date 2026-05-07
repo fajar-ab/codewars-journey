157bb3adb88f715b83ae7866ec6c7058d7cbae56
fajar-ab <fajarfanz378@gmail.com>
2026-05-08 01:24:43 +0700

    solve: Grasshopper - Check for factor (8 kyu)
    https://www.codewars.com/kata/reviews/5d78cd8ca4b23f0001b6cb9a/groups/5d7a1f7515a9070001105ebc

---
+++ 8-kyu/grasshopper_check_for_factor.py
@@ -0,0 +1,6 @@
+# Rank  : 8 kyu
+# Title : Grasshopper - Check for factor
+# Link  : https://www.codewars.com/kata/55cbc3586671f6aa070000fb
+
+def check_for_factor(base, factor):
+    return not bool(base % factor)
