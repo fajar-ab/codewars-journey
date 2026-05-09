30a9792bda63cdd27d83ee57185d56b069c26005
fajar-ab <fajarfanz378@gmail.com>
2026-05-10 02:20:45 +0700

    solve: Formatting decimal places #1 (7 kyu)
    https://www.codewars.com/kata/reviews/5642325fb20d8e84c20000bd/groups/5e2f6644191cfe00012cf78b

---
+++ 7-kyu/formatting_decimal_places_1.py
@@ -0,0 +1,6 @@
+# Rank  : 7 kyu
+# Title : Formatting decimal places #1
+# Link  : https://www.codewars.com/kata/5641c3f809bf31f008000042
+
+def two_decimal_places(number):
+    return int(number * 100) / 100
