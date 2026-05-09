1687c4cbb3e1011edabe5d539042603a6cd2355a
fajar-ab <fajarfanz378@gmail.com>
2026-05-10 01:52:26 +0700

    solve: Rot13 (5 kyu)
    https://www.codewars.com/kata/reviews/545683f329ccbfe984000087/groups/64dc57214621c90001bca192

---
+++ 5-kyu/rot13.py
@@ -0,0 +1,14 @@
+# Rank  : 5 kyu
+# Title : Rot13
+# Link  : https://www.codewars.com/kata/530e15517bc88ac656000716
+
+def rot13(text):
+    result = ""
+    for char in text:
+        if 'a' <= char <= 'z':
+            result += chr((ord(char) - ord('a') + 13) % 26 + ord('a'))
+        elif 'A' <= char <= 'Z':
+            result += chr((ord(char) - ord('A') + 13) % 26 + ord('A'))
+        else:
+            result += char
+    return result
