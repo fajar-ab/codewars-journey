0bcc88f364f95b5826f012a07cc8b43cc7f3fa46
fajar-ab <fajarfanz378@gmail.com>
2026-05-10 21:41:31 +0700

    refaktor: RGB To Hex Conversion (5 kyu)
    https://www.codewars.com/kata/reviews/54bd821b518b8d73630000a0/groups/54c559595acfc7da67001115

--- 5-kyu/rgb_to_hex_conversion.py
+++ 5-kyu/rgb_to_hex_conversion.py
@@ -3,4 +3,5 @@
 # Link  : https://www.codewars.com/kata/513e08acc600c94f01000001
 
 def rgb(r, g, b):
-    return "".join("{:02X}".format(max(0, min(255, value))) for value in [r, g, b])
+    round = lambda x: max(min(x, 255), 0)
+    return ("{:02X}" * 3).format(round(r), round(g), round(b))
