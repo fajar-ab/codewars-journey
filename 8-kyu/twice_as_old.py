da737a03f433e85e894976d9128d3ec2741649de
fajar-ab <fajarfanz378@gmail.com>
2026-05-11 19:22:37 +0700

    solve: Twice as old (8 kyu)
    https://www.codewars.com/kata/reviews/5b8533b67404e02cd7001407/groups/5b857a798179c621d6001708

---
+++ 8-kyu/twice_as_old.py
@@ -0,0 +1,6 @@
+# Rank  : 8 kyu
+# Title : Twice as old
+# Link  : https://www.codewars.com/kata/5b853229cfde412a470000d0
+
+def twice_as_old(dad_years_old, son_years_old):
+    return abs(dad_years_old - 2 * son_years_old)
