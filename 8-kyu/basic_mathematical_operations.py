3fef10b643b00a9b48e9da5d0f68a808af8bc9dd
fajar-ab <fajarfanz378@gmail.com>
2026-05-12 21:04:59 +0700

    solve: Basic Mathematical Operations (8 kyu)
    https://www.codewars.com/kata/reviews/57b15e5c8a36ac1d4600005c/groups/5bb48745b684d328d9000cb9

---
+++ 8-kyu/basic_mathematical_operations.py
@@ -0,0 +1,6 @@
+# Rank  : 8 kyu
+# Title : Basic Mathematical Operations
+# Link  : https://www.codewars.com/kata/57356c55867b9b7a60000bd7
+
+def basic_op(operator, value1, value2):
+    return eval(f"{value1} {operator} {value2}")
