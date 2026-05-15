1c3b9a0e7e27b21732628bbe2fa8846a3f07eae5
fajar-ab <fajarfanz378@gmail.com>
2026-05-15 19:58:43 +0700

    refaktor: Exes and Ohs (7 kyu)
    https://www.codewars.com/kata/reviews/5595c5b5998756bf60000099/groups/55964ae79ac002f67d0000c0

--- 7-kyu/exes_and_ohs.py
+++ 7-kyu/exes_and_ohs.py
@@ -2,13 +2,6 @@
 # Title : Exes and Ohs
 # Link  : https://www.codewars.com/kata/55908aad6620c066bc00002a
 
-from collections import Counter
-
 def xo(s):
-    value = s.lower()
-
-    if "x" not in value and "o" not in value:
-        return True
-
-    count = Counter(value)
-    return count["x"] == count["o"]
+    s = s.lower()
+    return s.count("x") == s.count("o")
