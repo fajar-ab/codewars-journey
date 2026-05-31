0761f29a051c6c1c878346cc2db8b641f702d801
fajar-ab <fajarfanz378@gmail.com>
2026-05-31 19:20:11 +0700

    solve: Sort Numbers (7 kyu)
    https://www.codewars.com/kata/reviews/54908ee1efb597f0ad00013d/groups/54908ee1efb597f0ad00013f

---
+++ 7-kyu/sort_numbers.py
@@ -0,0 +1,9 @@
+# Rank  : 7 kyu
+# Title : Sort Numbers
+# Link  : https://www.codewars.com/kata/5174a4c0f2769dd8b1000003
+
+def solution(nums):
+    return sorted(nums) if nums else []
+
+print(solution(None))
+print(solution([1,2,3,10,5]))
