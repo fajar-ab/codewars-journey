775d728a6df5ad5f0e8ead691bcf6fcbcf2d75b4
fajar-ab <fajarfanz378@gmail.com>
2026-05-31 19:33:00 +0700

    refaktor: Sort Numbers (7 kyu)
    https://www.codewars.com/kata/reviews/54908ee1efb597f0ad00013d/groups/54a3f96156a710f67d000caf

--- 7-kyu/sort_numbers.py
+++ 7-kyu/sort_numbers.py
@@ -2,8 +2,8 @@
 # Title : Sort Numbers
 # Link  : https://www.codewars.com/kata/5174a4c0f2769dd8b1000003
 
-def solution(nums):
-    return sorted(nums) if nums else []
+# menggunakan nilai operand
+solution = lambda nums: sorted(nums or [])
 
 print(solution(None))
 print(solution([1,2,3,10,5]))
