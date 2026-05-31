1a6c8f5f5bf0f818ee54e642ab8cf9a9d57d3411
fajar-ab <fajarfanz378@gmail.com>
2026-05-31 18:55:47 +0700

    sovle: 5 without numbers git status (8 kyu)
    https://www.codewars.com/kata/reviews/59441531563b3216cd00009a/groups/6a1c1f31192f5b1a38ee42d5

---
+++ 8-kyu/5_without_numbers.py
@@ -0,0 +1,14 @@
+# Rank  : 8 kyu
+# Title : 5 without numbers !!
+# Link  : https://www.codewars.com/kata/59441520102eaa25260000bf
+
+def unusual_five():
+    """
+    penta-
+    word-forming element in words of Greek origin or formation meaning "five, 
+    containing five," from Greek penta- (before a vowel pent-), combining form of 
+    pente "five," related to Aeolian pempte (from PIE root *penkwe- "five"), with -a- 
+    probably by analogy of hepta-, ennea-, deka-.
+    """
+
+    return len("PENTA")
