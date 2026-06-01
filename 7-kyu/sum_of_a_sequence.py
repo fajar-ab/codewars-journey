e321d75669623874abaeb72bb04f8830c96b2fcd
fajar-ab <fajarfanz378@gmail.com>
2026-06-01 12:52:32 +0700

    refaktor: Sum of a sequence (7 kyu)
    https://www.codewars.com/kata/reviews/587a9bf3e9665e4a4e00121a/groups/587aa802ef697da6a5000b46

--- 7-kyu/sum_of_a_sequence.py
+++ 7-kyu/sum_of_a_sequence.py
@@ -3,4 +3,4 @@
 # Link  : https://www.codewars.com/kata/586f6741c66d18c22800010a
 
 def sequence_sum(begin_number, end_number, step):
-    return sum(range(begin_number, end_number+1, step)) or 0
+    return sum(range(begin_number, end_number+1, step))
