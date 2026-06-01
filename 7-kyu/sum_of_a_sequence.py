3db18477928396180b7884caf5bc0ab6fcedc0fb
fajar-ab <fajarfanz378@gmail.com>
2026-06-01 12:50:50 +0700

    solve: Sum of a sequence (7 kyu)
    https://www.codewars.com/kata/reviews/587a9bf3e9665e4a4e00121a/groups/67be9771d32925fb1d7d1f80

---
+++ 7-kyu/sum_of_a_sequence.py
@@ -0,0 +1,6 @@
+# Rank  : 7 kyu
+# Title : Sum of a sequence
+# Link  : https://www.codewars.com/kata/586f6741c66d18c22800010a
+
+def sequence_sum(begin_number, end_number, step):
+    return sum(range(begin_number, end_number+1, step)) or 0
