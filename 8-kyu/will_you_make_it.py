2e66ac96ef2c5da1d1a9087a333a51d63e3f53da
fajar-ab <fajarfanz378@gmail.com>
2026-06-01 22:24:56 +0700

    solve: Will you make it? (8 kyu)
    https://www.codewars.com/kata/reviews/5861d292124b35723e000060/groups/58620b17dbbd8c369e000a5d

---
+++ 8-kyu/will_you_make_it.py
@@ -0,0 +1,6 @@
+# Rank  : 8 kyu
+# Title : Will you make it?
+# Link  : https://www.codewars.com/kata/5861d28f124b35723e00005e
+
+def zero_fuel(distance_to_pump, mpg, fuel_left):
+    return distance_to_pump <= mpg * fuel_left
