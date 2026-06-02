614a96791ae818bbc09a1f9d6e8abe13df5e41c7
fajar-ab <fajarfanz378@gmail.com>
2026-06-03 01:22:51 +0700

    refaktor: Remove anchor from URL (7 kyu)
    https://www.codewars.com/kata/reviews/54a5c2db74ff017dc5000093/groups/6a1f1d22e9058e138487a337

--- 7-kyu/remove_anchor_from_url.py
+++ 7-kyu/remove_anchor_from_url.py
@@ -2,9 +2,8 @@
 # Title : Remove anchor from URL
 # Link  : https://www.codewars.com/kata/51f2b4448cadf20ed0000386
 
+from urllib.parse import urldefrag
+
 def remove_url_anchor(url):
-    try: 
-        return url[:url.index("#")]
-    except ValueError:
-        return url
-    
+    result = urldefrag(url)
+    return result.url
