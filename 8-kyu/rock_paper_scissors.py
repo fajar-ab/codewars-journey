4c4659231f056814a06ecd8577a7052d1794d7b6
fajar-ab <fajarfanz378@gmail.com>
2026-06-04 02:17:55 +0700

    refaktor: Rock Paper Scissors! (8 kyu)
    https://www.codewars.com/kata/reviews/5672c704ea952f1e16000098/groups/5672f542cde4e12c0700008c

--- 8-kyu/rock_paper_scissors.py
+++ 8-kyu/rock_paper_scissors.py
@@ -3,12 +3,7 @@
 # Link  : https://www.codewars.com/kata/5672a98bdbdd995fad00000f
 
 def rps(p1, p2):
-    beats = {"rock": "scissors", "scissors": "paper", "paper": "rock"}
+    beats = {"rock": 0, "paper": 1, "scissors": 2}
+    result = ['Draw!', 'Player 1 won!', 'Player 2 won!']
     
-    if p1 == p2:
-        return "Draw!"
-    
-    if beats[p1] == p2:
-        return "Player 1 won!"
-    
-    return "Player 2 won!"
+    return result[beats[p1] - beats[p2]]
