5f6835c07ed159469718f2c9fad517da1c257365
fajar-ab <fajarfanz378@gmail.com>
2026-06-04 00:50:44 +0700

    solve: Product of consecutive Fib numbers (5 kyu)
    https://www.codewars.com/kata/reviews/5544e02c93ad59cef4000016/groups/65b1ea9d46c11000010e2d26

---
+++ 5-kyu/product_of_consecutive_fib_numbers.py
@@ -0,0 +1,11 @@
+# Rank  : 5 kyu
+# Title : Product of consecutive Fib numbers
+# Link  : https://www.codewars.com/kata/5541f58a944b85ce6d00006a
+
+def product_fib(_prod):
+    a, b = 0, 1
+
+    while a * b < _prod:
+        a, b = b, a + b
+
+    return [a, b, a * b == _prod]
