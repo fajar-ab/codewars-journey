4e0fa7d2b000fbde81d467d91b611795d0fb15bf
fajar-ab <fajarfanz378@gmail.com>
2026-06-03 13:21:09 +0700

    solve: Grasshopper - Messi goals function (8 kyu)
    https://www.codewars.com/kata/reviews/5da1b125e1cd9d0001541823/groups/5da1b21eb1415f00018d096d

---
+++ 8-kyu/grasshopper_messi_goals_function.py
@@ -0,0 +1,6 @@
+# Rank  : 8 kyu
+# Title : Grasshopper - Messi goals function
+# Link  : https://www.codewars.com/kata/55f73be6e12baaa5900000d4
+
+def goals(laLiga, copaDelRey, championsLeague):
+    return laLiga + copaDelRey + championsLeague
