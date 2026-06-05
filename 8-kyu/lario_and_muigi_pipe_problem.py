ffe57b62295f3efae135f11936dbebb1e5ba60f2
fajar-ab <fajarfanz378@gmail.com>
2026-06-06 01:43:48 +0700

    sovle: Lario and Muigi Pipe Problem (8 kyu)
    https://www.codewars.com/kata/reviews/56b7fbb90cf562bcaa00001f/groups/5ec257b234543e000189b294

---
+++ 8-kyu/lario_and_muigi_pipe_problem.py
@@ -0,0 +1,6 @@
+# Rank  : 8 kyu
+# Title : Lario and Muigi Pipe Problem
+# Link  : https://www.codewars.com/kata/56b29582461215098d00000f
+
+
+pipe_fix = lambda nums: list(range(nums[0], nums[-1] + 1))
