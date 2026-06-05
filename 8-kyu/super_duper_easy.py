0484a19e13c3042634a2d0e580c4de5038978135
fajar-ab <fajarfanz378@gmail.com>
2026-06-05 20:58:38 +0700

    solve: Super Duper Easy (8 kyu)
    https://www.codewars.com/kata/reviews/55a5bfb95c754688c0000219/groups/55a6427bd80baea8d300004e

---
+++ 8-kyu/super_duper_easy.py
@@ -0,0 +1,10 @@
+# Rank  : 8 kyu
+# Title : Super Duper Easy
+# Link  : https://www.codewars.com/kata/55a5bfaa756cfede78000026
+
+
+def problem(a):
+    try:
+        return a * 50 + 6
+    except TypeError:
+        return "Error"
