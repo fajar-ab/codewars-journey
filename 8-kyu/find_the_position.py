2227054249b53fa837b04e802ba1dc96a52bb170
fajar-ab <fajarfanz378@gmail.com>
2026-06-07 21:17:25 +0700

    solve: Find the position! (8 kyu)
    https://www.codewars.com/kata/reviews/580928f6d6bd8d2bfe00006e/groups/5e80f88e2472ea000170d111

---
+++ 8-kyu/find_the_position.py
@@ -0,0 +1,7 @@
+# Rank  : 8 kyu
+# Title : Find the position!
+# Link  : https://www.codewars.com/kata/5808e2006b65bff35500008f
+
+
+def position(letter):
+    return f"Position of alphabet: {ord(letter) - 96}"
