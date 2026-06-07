d8ebc344463ec38d483539e69255f3c73467549a
fajar-ab <fajarfanz378@gmail.com>
2026-06-07 20:50:06 +0700

    solve: Beginner Series #2 Clock (8 kyu)
    https://www.codewars.com/kata/reviews/56257c742fcbf7eb940000f0/groups/56257c742fcbf7eb940000f4

---
+++ 8-kyu/beginner_series_2_clock.py
@@ -0,0 +1,7 @@
+# Rank  : 8 kyu
+# Title : Beginner Series #2 Clock
+# Link  : https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a
+
+
+def past(h, m, s):
+    return (h * 3600 + m * 60 + s) * 1000
