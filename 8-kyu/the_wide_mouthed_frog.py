cd820a701efb9bd2b11db95890af92b662803e15
fajar-ab <fajarfanz378@gmail.com>
2026-06-07 19:28:11 +0700

    solve: The Wide-Mouthed frog! (8 kyu)
    https://www.codewars.com/kata/reviews/57ec923701c9e93a6000009b/groups/580131ca919127f096000522

---
+++ 8-kyu/the_wide_mouthed_frog.py
@@ -0,0 +1,7 @@
+# Rank  : 8 kyu
+# Title : The Wide-Mouthed frog!
+# Link  : https://www.codewars.com/kata/57ec8bd8f670e9a47a000f89
+
+
+def mouth_size(animal):
+    return "small" if animal.casefold() == "alligator" else "wide"
