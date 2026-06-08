6e3e3558ceb8651e2ca1954321e93efc47f25984
fajar-ab <fajarfanz378@gmail.com>
2026-06-08 17:47:36 +0700

    solve: Flatten and sort an array (7 kyu)
    https://www.codewars.com/kata/reviews/590230eb85ca615c3900009f/groups/5904bf692dad6bfe6a0009f1

---
+++ 7-kyu/flatten_and_sort_an_array.py
@@ -0,0 +1,10 @@
+# Rank  : 7 kyu
+# Title : Flatten and sort an array
+# Link  : https://www.codewars.com/kata/57ee99a16c8df7b02d00045f
+
+
+def flatten_and_sort(array):
+    return sorted(j for i in array for j in i)
+
+
+print(flatten_and_sort([[3, 2, 1], [7, 9, 8], [6, 4, 5]]))
