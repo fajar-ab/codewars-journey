197c852ae0bc17d7c00f2eb3cbf3dfb736c8c59f
fajar-ab <fajarfanz378@gmail.com>
2026-06-08 17:50:29 +0700

    refaktor: Flatten and sort an array (7 kyu)
    https://www.codewars.com/kata/reviews/590230eb85ca615c3900009f/groups/5902447a3ad56e2c67000126

--- 7-kyu/flatten_and_sort_an_array.py
+++ 7-kyu/flatten_and_sort_an_array.py
@@ -2,11 +2,9 @@
 # Title : Flatten and sort an array
 # Link  : https://www.codewars.com/kata/57ee99a16c8df7b02d00045f
 
-from itertools import chain
-
 
 def flatten_and_sort(array):
-    return sorted(chain(*array))
+    return sorted(sum(array, []))
 
 
 print(flatten_and_sort([[3, 2, 1], [7, 9, 8], [6, 4, 5]]))
