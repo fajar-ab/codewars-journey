2d4d96332d72b93011a937605cb96d8b68a96761
fajar-ab <fajarfanz378@gmail.com>
2026-06-08 13:34:21 +0700

    solve: Welcome! (8 kyu)
    https://www.codewars.com/kata/reviews/578408e3b7bca53e6d000200/groups/6a2661fdc36f997d444ebac8

---
+++ 8-kyu/welcome.py
@@ -0,0 +1,27 @@
+# Rank  : 8 kyu
+# Title : Welcome!
+# Link  : https://www.codewars.com/kata/577ff15ad648a14b780000e7
+
+
+def greet(language):
+    greeting = {
+        "english": "Welcome",
+        "czech": "Vitejte",
+        "danish": "Velkomst",
+        "dutch": "Welkom",
+        "estonian": "Tere tulemast",
+        "finnish": "Tervetuloa",
+        "flemish": "Welgekomen",
+        "french": "Bienvenue",
+        "german": "Willkommen",
+        "irish": "Failte",
+        "italian": "Benvenuto",
+        "latvian": "Gaidits",
+        "lithuanian": "Laukiamas",
+        "polish": "Witamy",
+        "spanish": "Bienvenido",
+        "swedish": "Valkommen",
+        "welsh": "Croeso",
+    }
+
+    return "Welcome" if language not in greeting else greeting.get(language)
