e2be8116a5140adc1b1aa2d4d78da3ef6a6fdd7c
fajar-ab <fajarfanz378@gmail.com>
2026-06-10 01:02:16 +0700

    solve: Welcome to the City (8 kyu)
    https://www.codewars.com/kata/reviews/559375be091cf8fa99000050/groups/5593d8954148f1d954000026

---
+++ 8-kyu/welcome_to_the_city.py
@@ -0,0 +1,7 @@
+# Rank  : 8 kyu
+# Title : Welcome to the City
+# Link  : https://www.codewars.com/kata/5302d846be2a9189af0001e4
+
+
+def say_hello(name, city, state):
+    return "Hello, {}! Welcome to {}, {}!".format(" ".join(name), city, state)
