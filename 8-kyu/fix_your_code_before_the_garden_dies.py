d760a8fead57fe2c0282b5a83326c0028103681a
fajar-ab <fajarfanz378@gmail.com>
2026-06-11 18:48:39 +0700

    solve: Fix your code before the garden dies! (8 kyu)
    https://www.codewars.com/kata/reviews/5716bac801b0bfa81c000049/groups/6a2a9f7e16a8f5e090c6070a

---
+++ 8-kyu/fix_your_code_before_the_garden_dies.py
@@ -0,0 +1,10 @@
+# Rank  : 8 kyu
+# Title : Fix your code before the garden dies!
+# Link  : https://www.codewars.com/kata/57158fb92ad763bb180004e7
+
+
+def rain_amount(mm):
+    if (rain_amount := int(mm)) < 40:
+        return f"You need to give your plant {40 - rain_amount}mm of water"
+    else:
+        return "Your plant has had more than enough water for today!"
