267b30fe6827ffa966a7c4489fd21dffa7e466b6
fajar-ab <fajarfanz378@gmail.com>
2026-06-12 22:52:51 +0700

    solve: Do you speak English? (8 kyu)
    https://www.codewars.com/kata/reviews/60760137bd7f6700015e6221/groups/610262355013010001e72292

---
+++ 8-kyu/do_you_speak_english.py
@@ -0,0 +1,7 @@
+# Rank  : 8 kyu
+# Title : Do you speak "English"?
+# Link  : https://www.codewars.com/kata/58dbdccee5ee8fa2f9000058
+
+
+def sp_eng(sentence):
+    return sentence.casefold().find("english") >= 0
