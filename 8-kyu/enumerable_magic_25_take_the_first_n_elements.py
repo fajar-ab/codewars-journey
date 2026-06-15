a5d03bdb30abd6141afa3608b437da84c146759f
fajar-ab <fajarfanz378@gmail.com>
2026-06-15 23:22:59 +0700

    solve: Enumerable Magic #25 - Take the First N Elements (8 kyu)
    https://www.codewars.com/kata/reviews/5d867105a48580000198fc33/groups/5d8671bab57008000115cbba

---
+++ 8-kyu/enumerable_magic_25_take_the_first_n_elements.py
@@ -0,0 +1,10 @@
+# Rank  : 8 kyu
+# Title : Enumerable Magic #25 - Take the First N Elements
+# Link  : https://www.codewars.com/kata/545afd0761aa4c3055001386
+
+
+def take(arr, n):
+    return arr[:n]
+
+
+print(take([1, 2, 3], 3))
