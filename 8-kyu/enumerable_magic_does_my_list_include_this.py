72828efc2f2dca85d29857304b5fade3383e72fc
fajar-ab <fajarfanz378@gmail.com>
2026-06-16 22:16:30 +0700

    solve: Enumerable Magic - Does My List Include This? (8 kyu)
    https://www.codewars.com/kata/reviews/5bc4d3d640ecc7c27b000680/groups/5bc4d3db168568e2f8000038

---
+++ 8-kyu/enumerable_magic_does_my_list_include_this.py
@@ -0,0 +1,7 @@
+# Rank  : 8 kyu
+# Title : Enumerable Magic - Does My List Include This?
+# Link  : https://www.codewars.com/kata/545991b4cbae2a5fda000158
+
+
+def include(arr, item):
+    return item in arr
