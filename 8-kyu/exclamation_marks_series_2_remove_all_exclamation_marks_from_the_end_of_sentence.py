933dc14b479f60b31bfe3878d4ed6cc8987c8d7b
fajar-ab <fajarfanz378@gmail.com>
2026-06-18 00:08:30 +0700

    solve: Exclamation marks series #2: Remove all exclamation marks from the end of sentence (8 kyu)
    https://www.codewars.com/kata/reviews/5804fa524c94b82b760000a4/groups/65903feb0dbd790001b73e43

---
+++ 8-kyu/exclamation_marks_series_2_remove_all_exclamation_marks_from_the_end_of_sentence.py
@@ -0,0 +1,7 @@
+# Rank  : 8 kyu
+# Title : Exclamation marks series #2: Remove all exclamation marks from the end of sentence
+# Link  : https://www.codewars.com/kata/57faece99610ced690000165
+
+
+def remove(st):
+    return st.rstrip("!")
