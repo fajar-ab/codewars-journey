5e7bd7fbcf1f1d8a9bc0993f93e853ebbb67463e
fajar-ab <fajarfanz378@gmail.com>
2026-06-18 00:11:45 +0700

    solve: No oddities here (8 kyu)
    https://www.codewars.com/kata/reviews/5e64acd8ba41250001567d5f/groups/5e68675ba7b774000129cf6f

---
+++ 8-kyu/no_oddities_here.py
@@ -0,0 +1,7 @@
+# Rank  : 8 kyu
+# Title : No oddities here
+# Link  : https://www.codewars.com/kata/51fd6bc82bc150b28e0000ce
+
+
+def no_odds(values):
+    return [number for number in values if number % 2 == 0]
