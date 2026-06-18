c7a2e506a953a081337d62ace455cd266ee4e018
fajar-ab <fajarfanz378@gmail.com>
2026-06-18 22:00:29 +0700

    solve: Reversing Words in a String (8 kyu)
    https://www.codewars.com/kata/reviews/57a7772702f095ae6d00017c/groups/57a7943ee298a7b6c60000b5

---
+++ 8-kyu/reversing_words_in_a_string.py
@@ -0,0 +1,7 @@
+# Rank  : 8 kyu
+# Title : Reversing Words in a String
+# Link  : https://www.codewars.com/kata/57a55c8b72292d057b000594
+
+
+def reverse(st):
+    return " ".join(reversed(st.split(" ")))
