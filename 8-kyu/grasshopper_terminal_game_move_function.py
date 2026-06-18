d1876fa9e1371ae7a0d797998b508d8c06f3e096
fajar-ab <fajarfanz378@gmail.com>
2026-06-18 22:50:28 +0700

    solve: Grasshopper - Terminal game move function (8 kyu)
    https://www.codewars.com/kata/reviews/5764a03d5274c658250011ef/groups/597087c420c8038d66000973

---
+++ 8-kyu/grasshopper_terminal_game_move_function.py
@@ -0,0 +1,7 @@
+# Rank  : 8 kyu
+# Title : Grasshopper - Terminal game move function
+# Link  : https://www.codewars.com/kata/563a631f7cbbc236cf0000c2
+
+
+def move(position, roll):
+    return position + roll * 2
