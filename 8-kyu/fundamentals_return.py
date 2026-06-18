70ed545a1d565aedf167a9937afa81067493f163
fajar-ab <fajarfanz378@gmail.com>
2026-06-18 20:02:30 +0700

    solve: Fundamentals: Return (8 kyu)
    https://www.codewars.com/kata/reviews/55a5bf4947d6bee9da0000d3/groups/55a5bf4947d6bee9da0000d5

---
+++ 8-kyu/fundamentals_return.py
@@ -0,0 +1,27 @@
+# Rank  : 8 kyu
+# Title : Fundamentals: Return
+# Link  : https://www.codewars.com/kata/55a5befdf16499bffb00007b
+
+
+def add(a, b):
+    return a + b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    return a / b
+
+
+def mod(a, b):
+    return a % b
+
+
+def exponent(a, b):
+    return a**b
+
+
+def subt(a, b):
+    return a - b
