5c71bc87156c6563b6ee9e037b0c2a00e20e6ff1
fajar-ab <fajarfanz378@gmail.com>
2026-06-19 23:27:14 +0700

    solve: Formatting decimal places #0 (8 kyu)
    https://www.codewars.com/kata/reviews/564220d56003fa369c000036/groups/5653c4cda764d92ae800004e

---
+++ 8-kyu/formatting_decimal_places_0.py
@@ -0,0 +1,7 @@
+# Rank  : 8 kyu
+# Title : Formatting decimal places #0
+# Link  : https://www.codewars.com/kata/5641a03210e973055a00000d/train/python
+
+
+def two_decimal_places(n):
+    return float(format(n, ".2f"))
