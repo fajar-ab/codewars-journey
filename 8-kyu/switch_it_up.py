43eb9fb5ffea0a4cf19dd677f49f09c7e74f57c1
fajar-ab <fajarfanz378@gmail.com>
2026-06-20 18:58:48 +0700

    solve: Switch it Up! (8 kyu)
    https://www.codewars.com/kata/reviews/580cfb2092a920b4b2000013/groups/580d050d4c7886c7680005ae

---
+++ 8-kyu/switch_it_up.py
@@ -0,0 +1,18 @@
+# Rank  : 8 kyu
+# Title : Switch it Up!
+# Link  : https://www.codewars.com/kata/5808dcb8f0ed42ae34000031
+
+
+def switch_it_up(number):
+    return {
+        0: "Zero",
+        1: "One",
+        2: "Two",
+        3: "Three",
+        4: "Four",
+        5: "Five",
+        6: "Six",
+        7: "Seven",
+        8: "Eight",
+        9: "Nine",
+    }.get(number)
