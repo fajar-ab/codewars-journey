68ab32a45db4e2dd0a03a243cebc0f8db3f97bf7
fajar-ab <fajarfanz378@gmail.com>
2026-06-21 20:35:26 +0700

    solve: Grasshopper - Combine strings (8 kyu)
    https://www.codewars.com/kata/reviews/5633f10cfb3281ba19000025/groups/5e09345e8c89df0001984315

---
+++ 8-kyu/grasshopper_combine_strings.py
@@ -0,0 +1,7 @@
+# Rank  : 8 kyu
+# Title : Grasshopper - Combine strings
+# Link  : https://www.codewars.com/kata/55f73f66d160f1f1db000059
+
+
+def combine_names(first, last):
+    return f"{first} {last}"
