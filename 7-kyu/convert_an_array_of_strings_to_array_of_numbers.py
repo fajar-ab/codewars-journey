2738fe74c0c27be12641b2efe4f128ca9736a5d2
fajar-ab <fajarfanz378@gmail.com>
2026-06-22 15:17:59 +0700

    solve: Convert an array of strings to array of numbers (7 kyu)
    https://www.codewars.com/kata/reviews/603f90d2a0bea400012cbb7c/groups/603f90d3a0bea400012cbb80

---
+++ 7-kyu/convert_an_array_of_strings_to_array_of_numbers.py
@@ -0,0 +1,7 @@
+# Rank  : 7 kyu
+# Title : Convert an array of strings to array of numbers
+# Link  : https://www.codewars.com/kata/5783d8f3202c0e486c001d23
+
+
+def to_float_array(arr):
+    return list(map(int, arr))
