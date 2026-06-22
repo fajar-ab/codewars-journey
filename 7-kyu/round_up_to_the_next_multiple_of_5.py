9eea79a48a2d24e4fc6495f6b189be2793032e20
fajar-ab <fajarfanz378@gmail.com>
2026-06-22 15:12:55 +0700

    solve: Round up to the next multiple of 5 (7 kyu)
    https://www.codewars.com/kata/reviews/55d1da126077327f9e000111/groups/563485f8c412858a90000003

---
+++ 7-kyu/round_up_to_the_next_multiple_of_5.py
@@ -0,0 +1,7 @@
+# Rank  : 7 kyu
+# Title : Round up to the next multiple of 5
+# Link  : https://www.codewars.com/kata/55d1d6d5955ec6365400006d
+
+
+def round_to_next5(n):
+    return ((n + 4) // 5) * 5
