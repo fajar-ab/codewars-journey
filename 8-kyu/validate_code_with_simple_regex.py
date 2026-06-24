231a08ab6b89050321518cdea8910d64a347d2b1
fajar-ab <fajarfanz378@gmail.com>
2026-06-24 19:05:20 +0700

    solve: validate code with simple regex (8 kyu)
    https://www.codewars.com/kata/reviews/56a29076db22b376ea000007/groups/56a4ac1ca4590f153c000011

---
+++ 8-kyu/validate_code_with_simple_regex.py
@@ -0,0 +1,7 @@
+# Rank  : 8 kyu
+# Title : validate code with simple regex
+# Link  : https://www.codewars.com/kata/56a25ba95df27b7743000016
+
+
+def validate_code(code):
+    return str(code)[0] in "123"
