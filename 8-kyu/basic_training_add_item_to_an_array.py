ea605de7ffc6b50a3079fa0ffbb216a6a32a7fc9
fajar-ab <fajarfanz378@gmail.com>
2026-06-24 19:48:54 +0700

    solve: Basic Training: Add item to an Array (8 kyu)
    https://www.codewars.com/kata/reviews/5758661640292ad941000002/groups/5758661940292ad941000008

---
+++ 8-kyu/basic_training_add_item_to_an_array.py
@@ -0,0 +1,7 @@
+# Rank  : 8 kyu
+# Title : Basic Training: Add item to an Array
+# Link  : https://www.codewars.com/kata/511f0fe64ae8683297000001
+
+
+# level mudah, tapi solusit sulit (baru nampak kata seperti ini!)
+websites.append("codewars")
