ef1b28d31c59e842117b67562c03d8486a7cd08c
fajar-ab <fajarfanz378@gmail.com>
2026-06-25 21:06:07 +0700

    solve: Bumps in the Road (7 kyu)
    https://www.codewars.com/kata/reviews/5809e499c5001d18740000ac/groups/5809e499c5001d18740000b0

---
+++ 7-kyu/bumps_in_the_road.py
@@ -0,0 +1,7 @@
+# Rank  : 7 kyu
+# Title : Bumps in the Road
+# Link  : https://www.codewars.com/kata/57ed30dde7728215300005fa
+
+
+def bumps(road):
+    return "Woohoo!" if road.count("n") <= 15 else "Car Dead"
