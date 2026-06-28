a94f5397a62f9cd7cd69c44c49c0fb5e8754da98
fajar-ab <fajarfanz378@gmail.com>
2026-06-29 00:03:22 +0700

    refaktor: Array.diff (6 kyu)
    https://www.codewars.com/kata/reviews/5520ac91933cd0a1560002d2/groups/5520cb79ecb433d4890003a0

--- 6-kyu/array_diff.py
+++ 6-kyu/array_diff.py
@@ -2,5 +2,15 @@
 # Title : Array.diff
 # Link  : https://www.codewars.com/kata/523f5d21c841566fde000009
 
+
 def array_diff(a, b):
     return [x for x in a if x not in b]
+
+
+a = [1, 2]
+b = [1]
+print(a, b, "=", array_diff(a, b))
+
+a = [1, 2, 2, 2, 3]
+b = [2]
+print(a, b, "=", array_diff(a, b))
