4222410357a32acd51f93dcc665c9325626ed67b
fajar-ab <fajarfanz378@gmail.com>
2026-07-01 20:19:42 +0700

    refaktor: Simple validation of a username with regex (8 kyu)
    https://www.codewars.com/kata/reviews/56a6420f333e5367f3000024/groups/56abdf5c54089fe96f000019

--- 8-kyu/simple_validation_of_a_username_with_regex.py
+++ 8-kyu/simple_validation_of_a_username_with_regex.py
@@ -3,7 +3,8 @@
 # Link  : https://www.codewars.com/kata/56a3f08aa9a6cc9b75000023
 
 
+import re
+
+
 def validate_usr(username):
-    return 4 <= len(username) <= 16 and all(
-        c.islower() or c.isdigit() or c == "_" for c in username
-    )
+    return bool(re.match(r"^[a-z0-9_]{4,16}$", username))
