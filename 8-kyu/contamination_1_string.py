ad936a742400ac549381d3937ea0e602c34f991b
fajar-ab <fajarfanz378@gmail.com>
2026-07-02 21:15:21 +0700

    solve: Contamination #1 -String- (8 kyu)
    https://www.codewars.com/kata/reviews/596fba6c74dd684086000004/groups/596fba6c74dd684086000006

---
+++ 8-kyu/contamination_1_string.py
@@ -0,0 +1,7 @@
+# Rank  : 8 kyu
+# Title : Contamination #1 -String-
+# Link  : https://www.codewars.com/kata/596fba44963025c878000039
+
+
+def contamination(text, char):
+    return char * len(text)
