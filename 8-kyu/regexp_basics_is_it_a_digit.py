507164a9ad4b4189f35817775ac9866bf3c3e40a
fajar-ab <fajarfanz378@gmail.com>
2026-07-02 20:56:54 +0700

    solve: Regexp Basics - is it a digit? (8 kyu)
    https://www.codewars.com/kata/reviews/569c0ab2269589c328000047/groups/56a40184289bdedc84000013

---
+++ 8-kyu/regexp_basics_is_it_a_digit.py
@@ -0,0 +1,7 @@
+# Rank  : 8 kyu
+# Title : Regexp Basics - is it a digit?
+# Link  : https://www.codewars.com/kata/567bf4f7ee34510f69000032
+
+
+def is_digit(n):
+    return len(n) == 1 and n.isdigit()
