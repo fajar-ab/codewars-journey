3c091e81cc62f8c07183f0dce88992104fca49d5
fajar-ab <fajarfanz378@gmail.com>
2026-07-04 23:05:38 +0700

    solve: Remove the time (8 kyu)
    https://www.codewars.com/kata/reviews/56c5cdb06c6021354800008d/groups/56c62b8d8046eccd24000d64

---
+++ 8-kyu/remove_the_time.py
@@ -0,0 +1,7 @@
+# Rank  : 8 kyu
+# Title : Remove the time
+# Link  : https://www.codewars.com/kata/56b0ff16d4aa33e5bb00008e
+
+
+def shorten_to_date(long_date):
+    return long_date[: long_date.index(",")]
