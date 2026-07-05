c07639225af22b24ac9fa4d6e62c4ce27c2d6f31
fajar-ab <fajarfanz378@gmail.com>
2026-07-05 22:35:33 +0700

    solve: Template Strings (8 kyu)
    https://www.codewars.com/kata/reviews/605f31e347d5d40001b37599/groups/605f31e347d5d40001b3759d

---
+++ 8-kyu/template_strings.py
@@ -0,0 +1,7 @@
+# Rank  : 8 kyu
+# Title : Template Strings
+# Link  : https://www.codewars.com/kata/55a14f75ceda999ced000048
+
+
+def temple_strings(obj, feature):
+    return f"{obj} are {feature}"
