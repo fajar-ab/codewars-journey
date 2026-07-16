0be8f85a04d670167b8058872a8ec6bb0b44479b
fajar-ab <fajarfanz378@gmail.com>
2026-07-16 22:04:56 +0700

    solve: Substituting Variables Into Strings: Padded Numbers (7 kyu)
    https://www.codewars.com/kata/reviews/5d0bee879bde3400017e5ec8/groups/5d0c166ea535d50001cbf65a

---
+++ 7-kyu/substituting_variables_into_strings_padded_numbers.py
@@ -0,0 +1,7 @@
+# Rank  : 7 kyu
+# Title : Substituting Variables Into Strings: Padded Numbers
+# Link  : https://www.codewars.com/kata/51c89385ee245d7ddf000001
+
+
+def solution(value):
+    return f"Value is {value:05}"
